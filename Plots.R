9b64d4d26ee7bdefeb3373b9ef82eaa7f65537dc
Aritra Ghosal <56138318+aghosal89@users.noreply.github.com>
2022-12-06 17:57:56 -0800

    Add files via upload

--- Plots.R
+++ Plots.R
@@ -4,7 +4,8 @@ library("latex2exp")
 library("survey")
 library("fda.usc")
 library("viridis")
-library("tikzDevice")
+library("ggplot2")
+
 # Set library for R oprations
 setwd("~/Documents/FSI/Application /alex/FSI_NHANES/Archive_PLFSI")
 
@@ -125,7 +126,7 @@ plotdf_NHA<- data.frame(Coefficients= as.vector(rbind(matrix(0, nrow=length(tt1)
                         CI= rep(c("0","Estimate","Upper 95%","Lower 95%"), 
                         each= length(tt1)))
 
-plotdf_NHM<- data.frame(Coefficients= as.vector(rbind(matrix(0, nrow=length(tt1), 
+plotdf_ORIMR<- data.frame(Coefficients= as.vector(rbind(matrix(0, nrow=length(tt1), 
                       ncol=1),t(b1[7,]), t(b_u95_1[7,]), t(b_l95_1[7,]))),
                         t=rep(tt1, 4), 
                         CI= rep(c("0","Estimate","Upper 95%","Lower 95%"), 
@@ -138,24 +139,39 @@ plotdf_HEI<- data.frame(Coefficients= as.vector(rbind(matrix(0, nrow=length(tt1)
                         each= length(tt1)))
 
 plotdf<- data.frame(rbind(plotdf_intercept, plotdf_SexF, plotdf_OH, plotdf_NHW,
-                          plotdf_NHB, plotdf_NHA, plotdf_HEI), 
+                          plotdf_NHB, plotdf_NHA, plotdf_ORIMR, plotdf_HEI), 
                   Coefficient=rep(c("Intercept", "Sex:Female","Other Hispanic",
                     "Non Hispanic White", "Non Hispanic Black",
-                   "Non Hispanic Asian", "HEI"), each= nrow(plotdf_intercept)))
+                   "Non Hispanic Asian", "Other Races(incl. Multi-racial)", "HEI"), each= nrow(plotdf_intercept)),
+                  Ord = rep(c(1:8), each= nrow(plotdf_intercept)))
 
-postscript(file = "Rplot_betafunctional_linear2.eps", width = 8, height = 8, paper = 'special')
+blank_data <- data.frame(Coefficient=rep(c("Intercept", "Sex:Female","Other Hispanic",
+                                           "Non Hispanic White", "Non Hispanic Black",
+                                           "Non Hispanic Asian", "Other Races(incl. Multi-racial)",
+                                           "HEI"), each=2), t = 0, 
+                         Coefficients = c(min(b_l95_1[1,]), max(b_u95_1[1,]), 
+                               min(b_l95_1[2,]), max(b_u95_1[2,]),
+                               min(b_l95_1[c(3:7),]), max(b_u95_1[c(3:7),]),
+                               min(b_l95_1[c(3:7),]), max(b_u95_1[c(3:7),]),
+                               min(b_l95_1[c(3:7),]), max(b_u95_1[c(3:7),]),
+                               min(b_l95_1[c(3:7),]), max(b_u95_1[c(3:7),]),
+                               min(b_l95_1[c(3:7),]), max(b_u95_1[c(3:7),]),
+                               min(b_l95_1[8,]), max(b_u95_1[8,])),
+                         CI= rep(c("Upper 95%","Lower 95%"), 8),
+                         Ord=1)
 
 ggplot(data = plotdf, aes(x=t, y=Coefficients, fill=CI)) +
   geom_path(aes(colour=as.factor(CI)), size=.9, alpha=1) +
   labs(colour = "Coefficient") +
   scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9", "#009E73")) +
-  facet_wrap(~Coefficient, scales = "free") +
+  facet_wrap( ~reorder(Coefficient, Ord), scales = "free_y") +
   theme(axis.text=element_text(size=11),
         axis.title=element_text(size=11)) +
   theme(text=element_text(size=11)) +
-  ylab(label = "Estimated Coefficients") 
+  geom_blank(data = blank_data, aes(x = t, y = Coefficients)) +
+  ylab(label = "Estimated Coefficients") + theme_bw() 
 
-dev.off()
+ggsave(file= "Rplot_betafunctional_linear2.eps", width = 8, height = 7) 
 
 ###########################################
 # Codes to create figure 3 in the document
@@ -222,8 +238,8 @@ Theta_hat<- th
 
 q50<- floor(which(tt>=0.50))[1]   # check index for 50th percentile 
 q75<- floor(which(tt>=0.75))[1]   # check index for 75th percentile
-q80<- floor(which(tt>=0.80))[1]   # check index for 80th percentile
-q90<- floor(which(tt>=0.90))[1]   # check index for 90th percentile
+q90<- floor(which(tt>=0.90))[1]   # check index for 80th percentile
+q97<- floor(which(tt>=0.97))[1]   # check index for 90th percentile
 
 sp <- 4 
 dfs<- 9
@@ -244,7 +260,7 @@ x_males_eth1<- cbind.data.frame(Intercept=1, HEI=median(datosx$HEI), bs_data1)
 yh_males_eth1<- matrix(NA, nrow = dim(x_males_eth1)[1], ncol = length(tt))
 
 for (r in 1:length(tt)) {
-  yh_males_eth1[,r] <- as.matrix(x_males_eth1)%*% matrix(b[,r][-c(2:7)],ncol=1)
+  yh_males_eth1[,r] <- as.matrix(x_males_eth1)%*% matrix(b[,r][c(1,8:17)],ncol=1)
 }
 
 source("cuadratico.R", local = knitr::knit_global())
@@ -258,15 +274,13 @@ for(j in 1:length(indt)) {
 proc.time() - t
 
 # save the quantiles for future use 
-Yh<- data.frame(yh_males_eth1[,c(q50, q75, q80, q90)])
-colnames(Yh)<- c("Q50","Q75","Q80","Q90")
-write.csv(yh_males_eth1, "Pedicted_percentiles.csv")
+write.csv(yh_males_eth1, "Pedicted_transport_percentiles.csv")
+Yh<- as.data.frame(yh_males_eth1[,c(q50, q75, q90, q97)])
+colnames(Yh)<- c("Q50","Q75","Q90","Q97")
 
-yh_males_eth1$Integral<- sapply(1:nrow(yh_males_eth1), 
-        function(i) fdadensity:::trapzRcpp(X = tt, Y = as.numeric(yh_males_eth1[i,])))
+#yh_males_eth1$Integral<- sapply(1:nrow(yh_males_eth1), 
+#        function(i) fdadensity:::trapzRcpp(X = tt, Y = as.numeric(yh_males_eth1[i,])))
   
-Yh<- data.frame(read.csv("Pedicted_percentiles.csv")[,c(q50, q75, q80, q90)])
-
 df_q50<- cbind.data.frame(BMI=tsd_grid_level$BMI,
                        AGE=tsd_grid_level$Age,
                        Quantiles=Yh$Q50)
@@ -275,16 +289,13 @@ df_q75<- cbind.data.frame(BMI=tsd_grid_level$BMI,
                           AGE=tsd_grid_level$Age,
                           Quantiles=Yh$Q75)
 
-df_q80<- cbind.data.frame(BMI=tsd_grid_level$BMI,
-                          AGE=tsd_grid_level$Age,
-                          Quantiles=Yh$Q80)
-
 df_q90<- cbind.data.frame(BMI=tsd_grid_level$BMI,
                           AGE=tsd_grid_level$Age,
                           Quantiles=Yh$Q90)
 
-setEPS()
-postscript(file = "Rplot_quantile50_prediction.eps", width = 6.5, height = 5.5, paper = 'special')
+df_q97<- cbind.data.frame(BMI=tsd_grid_level$BMI,
+                          AGE=tsd_grid_level$Age,
+                          Quantiles=Yh$Q97)
 
 ggplot(df_q50, aes(x=BMI, y=AGE)) +
   geom_raster(aes(fill = Quantiles)) +
@@ -293,10 +304,8 @@ ggplot(df_q50, aes(x=BMI, y=AGE)) +
       axis.title=element_text(size=15)) +
   theme(text=element_text(size=15)) +
   ggtitle("50th Quantile")
+ggsave(file = "Rplot_quantile50_prediction.eps", width = 6.5, height = 5.5)
 
-dev.off()
-
-postscript(file = "Rplot_quantile75_prediction.eps", width = 6.5, height = 5.5, paper = 'special')
 
 ggplot(df_q75, aes(x=BMI, y=AGE)) +
   geom_raster(aes(fill = Quantiles)) +
@@ -305,36 +314,48 @@ ggplot(df_q75, aes(x=BMI, y=AGE)) +
         axis.title=element_text(size=15)) +
   theme(text=element_text(size=15)) +
   ggtitle("75th Quantile")
+ggsave(file = "Rplot_quantile75_prediction.eps", width = 6.5, height = 5.5)
 
-dev.off()
 
-postscript(file = "Rplot_quantile80_prediction.eps", width = 6.5, height = 5.5, paper = 'special')
-
-ggplot(df_q80, aes(BMI, AGE)) +
-  geom_raster(aes(fill = Quantiles)) +
-  scale_fill_viridis() +
-  theme(axis.text=element_text(size=15),
-        axis.title=element_text(size=15)) +
-  theme(text=element_text(size=15)) +
-  ggtitle("80th Quantile")
-
-dev.off()
-
-postscript(file = "Rplot_quantile90_prediction.eps", width = 6.5, height = 5.5, paper = 'special')
-
-ggplot(df_q90, aes(x=BMI, y=AGE)) +
+ggplot(df_q90, aes(BMI, AGE)) +
   geom_raster(aes(fill = Quantiles)) +
   scale_fill_viridis() +
   theme(axis.text=element_text(size=15),
         axis.title=element_text(size=15)) +
   theme(text=element_text(size=15)) +
   ggtitle("90th Quantile")
-
-dev.off()
-
+ggsave(file = "Rplot_quantile90_prediction.eps", width = 6.5, height = 5.5)
 
 
+ggplot(df_q97, aes(x=BMI, y=AGE)) +
+  geom_raster(aes(fill = Quantiles)) +
+  scale_fill_viridis() +
+  theme(axis.text=element_text(size=15),
+        axis.title=element_text(size=15)) +
+  theme(text=element_text(size=15)) +
+  ggtitle("97th Quantile")
+ggsave(file = "Rplot_quantile97_prediction.eps", width = 6.5, height = 5.5)
 
+
+########
+# Compute the integral of the quantiles
+
+yh_males_eth1_int<- sapply(1:nrow(yh_males_eth1), function(i) {
+  fdadensity:::trapzRcpp(X=tt, Y= yh_males_eth1[i,])
+})
+
+df_int<- cbind.data.frame(BMI=tsd_grid_level$BMI,
+                          AGE=tsd_grid_level$Age,
+                          Integrals=yh_males_eth1_int)
+
+ggplot(df_int, aes(x=BMI, y=AGE)) +
+  geom_raster(aes(fill = Integrals)) +
+  scale_fill_viridis() +
+  theme(axis.text=element_text(size=15),
+        axis.title=element_text(size=15)) +
+  theme(text=element_text(size=15)) +
+  ggtitle("Integated Quantiles")
+ggsave(file = "Rplot_quantile_integrals.eps", width = 6.5, height = 5.5)
 
 ###########################################
 # Codes to create figure 4 in the document
@@ -345,7 +366,6 @@ m2<- 60
 
 # read the functional beta coefficients
 b= read.csv("Output_Age20to80_noTAC_betas3.csv", header = T)[,-1]
-#beta_functional_coeffs<- b
 
 # read the covaraiets in the PLFSI model
 rnames<- read.csv("Output_Age20to80_noTAC_rnames3.csv", header = T)[,-1]
@@ -362,7 +382,7 @@ datos<- subset(datos, datos$RIDAGEYR.x >= 20 & datos$RIDAGEYR.x <= 80)
 
 # subset data for BMI 15-40
 datos<- subset(datos, datos$BMXBMI >= 18.5 & datos$BMXBMI <= 40)  
-datos<- datos[-c(734, 1216, 2094, 2699, 2958, 3092, 3933, 3963, 4232), ]
+#datos<- datos[-c(734, 1216, 2094, 2699, 2958, 3092, 3933, 3963, 4232), ]
 
 datosfda= datos[,188:687]
 datosfda= as.matrix(datosfda)
@@ -405,43 +425,16 @@ temp_si_data<- data.frame(BMXBMI= seq(min(datosx$BMXBMI),max(datosx$BMXBMI), len
 tsd_grid<- expand.grid(temp_si_data)
 tsd_grid_level<- expand.grid(data.frame(BMI=x_, Age=y_))
 
-# read the estimated index parameter
-th<- matrix(read.csv("Theta_Hat.csv")[,-1], ncol =1)
-Theta_hat<- th
+# univariate
+u_2<- as.matrix(cbind(datosx$BMXBMI, datosx$RIDAGEYR.y))%*%th
+u_2 <- sort(u_2)
 
-u_t
-u_<- as.matrix(tsd_grid)%*%th
-# fix the order of the B-spline basis 
-sp <- 4 
-
-# fix the degrees of freedom
-dfs<- 9
-
+aspl1.1<- splines::bs(u_2, df= dfs, knots = NULL, degree = sp, 
+                      intercept = FALSE)
 
 #####################
 # The following are the bondary knots and the rest of the knot sequence for the 
 # spline basis computation
-
-aspl<- splines2::dbs(x=u_, derivs = 1L, df= dfs, knots = NULL, degree = sp, 
-                     intercept = FALSE)
-
-drv<- matrix(NA, nrow =nrow(aspl), ncol =length(tt))
-for (r in 1:length(tt)) {
-  drv[,r] <- aspl%*% matrix(b[,r][-c(1:8)],ncol=1)
-}
-
-q50<- floor(which(tt>=0.50))[1]   # check index for 50th percentile 
-q75<- floor(which(tt>=0.75))[1]   # check index for 75th percentile
-q80<- floor(which(tt>=0.80))[1]   # check index for 80th percentile
-q90<- floor(which(tt>=0.90))[1]   # check index for 90th percentile
-
-
-# univariate
-u_2<- as.matrix(cbind(datosx$BMXBMI, datosx$RIDAGEYR.y))%*%th
-u_2 <- sort(u_2)
-#u_2<- seq(min(u_d), max(u_d), len=200)
-aspl1.1<- splines::bs(u_2, df= dfs, knots = NULL, degree = sp, 
-                      intercept = FALSE)
 
 bknots<- attributes(aspl1.1)$Boundary.knots
 knots <- attributes(aspl1.1)$knots
@@ -451,16 +444,15 @@ aspl2<- splines2::dbs(x=u_2, derivs = 1L, df= dfs, knots = knots,
                       degree = sp, Boundary.knots= bknots,
                       intercept = FALSE)
 
-
-# check index for 15th percentile of the Single Index
+# check index for 16.6667th percentile of the Single Index
 row_ind_fbo<- floor(which(u_2>=knots_all[2]))[1]
   
-# check index for 83rd percentile of the Single Index
+# check index for 83.3333rd percentile of the Single Index
 row_ind_lbo<- floor(which(u_2>=knots_all[length(knots_all)-1]))[1]
   
 drv2<- matrix(NA, nrow =nrow(aspl2), ncol =length(tt))
 for (r in 1:length(tt)) {
-  drv2[,r] <- aspl2%*% matrix(b[,r][-c(1:8)],ncol=1)
+  drv2[,r] <- aspl2%*% matrix(b[,r][c(9:17)],ncol=1)
 }
 
 df_in <- drv2[c(row_ind_fbo:row_ind_lbo),]
@@ -474,47 +466,43 @@ df_boundary_r <- drv2[c(row_ind_lbo:length(u_2)),]
 setEPS()
 postscript(file = "Rplot_qderiv_SI.eps", width = 5, height = 5, paper = 'special')
 
+#"#E69F00", "#56B4E9", "#009E73", "#0072B2"
+
 plot(u_2_in, df_in[,q50], type ="l", ylab ="Derivative of Quantiles", 
-     xlab="Single Index", ylim=c(-10,25), xlim =c(-2.5,2.7), lwd=3)
-lines(u_2_in, df_in[,q75], col="green", lwd=3)
-lines(u_2_in, df_in[,q80], col="blue", lwd=3)
-lines(u_2_in, df_in[,q90], col="yellow2", lwd=3)
+     xlab="Single Index", ylim=c(-10,25), xlim =c(-2.5,2.7), lwd=1.5, col="#000000")
+lines(u_2_in, df_in[,q75], col="#D55E00", lwd=2)
+lines(u_2_in, df_in[,q90], col="#009E73", lwd=2.5)
+lines(u_2_in, df_in[,q97], col="#0072B2", lwd=3)
 
-lines(u_2_boundary_l, df_boundary_l[,q50], lwd=3, lty=3)
-lines(u_2_boundary_l, df_boundary_l[,q75], col="green", lwd=3, lty =3)
-lines(u_2_boundary_l, df_boundary_l[,q80], col="blue", lwd=3, lty =3)
-lines(u_2_boundary_l, df_boundary_l[,q90], col="yellow2", lwd=3, lty =3)
+lines(u_2_boundary_l, df_boundary_l[,q50], lwd=1.5, lty=3, col="#000000")
+lines(u_2_boundary_l, df_boundary_l[,q75], col="#D55E00", lwd=2, lty =3)
+lines(u_2_boundary_l, df_boundary_l[,q90], col="#009E73", lwd=2.5, lty =3)
+lines(u_2_boundary_l, df_boundary_l[,q97], col="#0072B2", lwd=3, lty =3)
 
-lines(u_2_boundary_r, df_boundary_r[,q50], lwd=3, lty=3)
-lines(u_2_boundary_r, df_boundary_r[,q75], col="green", lwd=3, lty =3)
-lines(u_2_boundary_r, df_boundary_r[,q80], col="blue", lwd=3, lty =3)
-lines(u_2_boundary_r, df_boundary_r[,q90], col="yellow2", lwd=3, lty =3)
+lines(u_2_boundary_r, df_boundary_r[,q50], lwd=1.5, lty=3, col="#000000")
+lines(u_2_boundary_r, df_boundary_r[,q75], col="#D55E00", lwd=2, lty =3)
+lines(u_2_boundary_r, df_boundary_r[,q90], col="#009E73", lwd=2.5, lty =3)
+lines(u_2_boundary_r, df_boundary_r[,q97], col="#0072B2", lwd=3, lty =3)
 
 for (j in 1:length(knots_all)) {
   abline(v=knots_all[j], lty=3, col="red")
 }
 
-legend(-1, 25, legend=c("t=0.50","t=0.75","t=0.80","t=0.90"),
-       col=c("black","green","blue","yellow2"), 
-       lwd =c(3,3,3,3),
-       lty=c(1,1,1,1), cex=1.2, text.font=10
-       #, bg='lightgrey'
-)
-grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
-     lwd = par("lwd"), equilogs = TRUE)
+legend(-1, 25, legend=c("t=0.50","t=0.75","t=0.90","t=0.97"),
+       col=c("#000000", "#D55E00", "#009E73", "#0072B2"), 
+       lwd =c(1.5,2,2.5,3),
+       lty=c(1,1,1,1), cex=1.2, text.font=10,
+       bg='lightgrey')
+
+#grid(nx = NULL, ny = NULL, col = "lightgray", lty = "dotted",
+#     lwd = par("lwd"), equilogs = TRUE)
 
 dev.off()
 
 
 postscript(file = "Rplot_histogram_ofSI.eps", width = 4.5, height = 4.5, paper = 'special')
 
-hist(u_d, freq=FALSE, main="Histogram of Single Index", xlab= "")
-dev.off()
-
-postscript(file = "Rplot_kdens_SI.eps", width = 4.5, height = 4.5, paper = 'special')
-
-plot(density(u_d), main="")
-abline(v=0, lty=3, col="red")
+hist(u_2, freq=FALSE, main="Histogram of Single Index", xlab= "")
 dev.off()
 
 
