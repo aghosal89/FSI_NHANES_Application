b9a534c8e3f5d69766ab3d1e03f4ce956a5b0c87
Aritra Ghosal <56138318+aghosal89@users.noreply.github.com>
2022-12-06 16:46:03 -0800

    Add files via upload

--- survey2wassersteinmodel.R
+++ survey2wassersteinmodel.R
@@ -6,31 +6,41 @@
 
 ## Outputs:1) r^2               - the R^2 of the fitted model
 ##         2) betaj             - beta coefficients of the fitted regression model
-##         3) predicciones      - the prediction of regression model
-##         4) residuos          - the residuals of the fitted model
+##         3) beta_lcl          - pointwise lower 95% Confidence Interval of the beta parameter
+##         4) beta_ucl          - pointwise upper 95% Confidence Interval of the beta parameter
+##         3) predicciones      - the prediction of regression model after projection to 
+##                                L^2-Wasserstein space.
+##         4) residuos          - the residuals of the fitted model. 
+##         5) r2vec             - a vector of length same as number of columns in predicciones 
+##                                above, containing the multiple R-squared for each percentile.
 
 
 survey2wassersteinmodel <- function(formula=formula, data_analysis_svy=data_analysis_svy, objetofda= objetofda) {
   library("survey")
   
   prediciones = matrix(0, nrow= dim(data_analysis_svy$variables)[1], ncol= ncol(objetofda$data))
-  residuos = prediciones
-  
+  residuos <- prediciones
+  residuos2 <- prediciones
+  r2vec<- rep(NA, dim(prediciones)[2])
   formulaaux= paste("X", 1, sep="")
   formulaaux= paste(formulaaux, "~", sep="")
   
   formulafinal= paste(formulaaux, formula,sep="")
   formulafinal= as.formula(formulafinal)  
   
-  m2= svyglm(formulafinal,design=data_analysis_svy, family=stats::gaussian())
-  
+  m2= svyglm(formulafinal, design=data_analysis_svy, family=stats::gaussian())
+  #print(summary(m2))
   betaj= matrix(0, nrow=length(m2$coefficients), ncol=ncol(objetofda$data))
   betaj_ucl= matrix(0, nrow=length(m2$coefficients), ncol=ncol(objetofda$data))
   betaj_lcl= matrix(0, nrow=length(m2$coefficients), ncol=ncol(objetofda$data))
   
+  # obtain the survey weights
+  w= data_analysis_svy$variables$survey_wt
+  w= w/sum(w)
+  
   #setup parallel backend to use many processors
   t= proc.time()
-  for(i in 1:ncol(objetofda$data)){
+  for(i in 1:ncol(objetofda$data)) {
     
     formulaaux= paste("X", i, sep="")
     formulaaux= paste(formulaaux,"~",sep="")
@@ -48,24 +58,42 @@ survey2wassersteinmodel <- function(formula=formula, data_analysis_svy=data_anal
     prediciones[,i]= as.numeric(m$fitted.values)
     residuos[,i]= aux
     
+    # Compute the Multiple R-squared for each quantile:
+    y_mean<- sum(w*objetofda$data[,i])
+    TSS<- sum(w*(objetofda$data[,i] - y_mean)^2)
+    if(TSS==0) {
+      r2vec[i] <- 1
+    } else {
+      SSE<- sum(w*(aux)^2)
+      r2vec[i] <- 1- (SSE/TSS)
+    }
   }
   print(t - proc.time())
   
-  pesosestandarizados= data_analysis_svy$variables$survey_wt
-  pesosestandarizados= pesosestandarizados/sum(pesosestandarizados)
+  # check if the predictions are quantiles
+  source("cuadratico.R", local = knitr::knit_global())
+  indt <- apply(prediciones, 1, function(x) {all(diff(x)>= 0)==FALSE})
+  for(j in 1:length(indt)) {
+    if(indt[j]==TRUE){
+      prediciones[j,]<- cuadratico(matrix(prediciones[j,], nrow = 1))
+    }
+  }
   
-  media1= func.mean(objetofda)
-  residuos2= objetofda-media1
+  media1 = apply(w*objetofda$data, 2, sum)
+  for (i in 1:nrow(objetofda$data)) {
+    residuos2[i,]<- objetofda$data[i,]- media1
+  }
   
-  disp1= apply(residuos2$data, 1, function(x){sum(x^2)})
+  disp1= apply(residuos2, 1, function(x){sum(x^2)})
   disp2= apply(residuos, 1, function(x){sum(x^2)})
   
-  # I will must update this with survey weights
+  # The R-squared takes into account the survey weights
   
-  r2 <- 1-(sum(disp2*pesosestandarizados)/sum(disp1*pesosestandarizados))
+  r2 <- 1-(sum(disp2*w)/sum(disp1*w))
   
   rownames(betaj) <- names(m$coefficients)
   
-  return(list("r2"=r2,"betaj"=betaj, "beta_lcl"=betaj_lcl, "beta_ucl"=betaj_ucl, "predicciones"= prediciones,"residuos"= residuos))
+  return(list("r2"=r2,"betaj"=betaj, "beta_lcl"=betaj_lcl, "beta_ucl"=betaj_ucl, 
+              "predicciones"= prediciones,"residuos"= residuos, "R2_vector"=r2vec))
   
 }
