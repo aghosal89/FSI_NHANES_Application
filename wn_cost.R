ba3b84488d15bcd232dc89e3bc8013ead41858ad
Aritra Ghosal <56138318+aghosal89@users.noreply.github.com>
2022-12-06 16:45:15 -0800

    Add files via upload

--- wn_cost.R
+++ wn_cost.R
@@ -1,24 +1,25 @@
 
 ## This is the cost function for estimating the index parameter
 
-## Inputs: 1) et            - index parameter in polar coordinates
-##         2) datosfda      - an nxm matrix as the response whose each row represents an observation, 
-##                            each column represents a quantile, t from [0,1].
-##         3) linear_vars   - the names of the p covariates for the linear part of model included in datosx 
-##         4) si_vars       - the names of the q covariates for the SI part of model included in datosx
-##         5) tt            - the equidistant grid on [0,1] of length m.
-##         6) sp            - the degree of polynomial considered for spline regression.
-##         8) dfs           - degrees of freedom as an alternative to specifying the knots. 
-##         9) datosx        - a data frame with n rows whose columns include the covariates for the model
-##                            as well as for survey design.
+## Inputs: 
 
+## 1) et            - index parameter in polar coordinates
+## 2) datosfda      - an nxm matrix as the response whose each row represents an observation, 
+##                    each column represents a quantile, t from [0,1].
+## 3) linear_vars   - the names of the p covariates for the linear part of model included in datosx 
+## 4) si_vars       - the names of the q covariates for the SI part of model included in datosx
+## 5) formula_lv    - a character of length=1, the formula of the covariates in the linear part
+## 5) tt            - the equidistant grid on [0,1] of length m.
+## 6) sp            - the degree of polynomial considered for spline regression.
+## 7) dfs           - degrees of freedom as an alternative to specifying the knots. 
+## 8) datosx        - a data frame with n rows whose columns include the covariates for the model
+##                    as well as for survey design.
 
 ## Output: the mean square prediction error. 
 
-
-
-wn_cost <- function(et, datosfda, linear_vars, si_vars, tt, sp, dfs, datosx) {
+wn_cost <- function(et, datosfda, linear_vars, si_vars, formula_lv, tt, sp, dfs, datosx) {
   
+  # read the necessary libraries:
   library("survey")
   library("splines")
   library("fda.usc")
@@ -37,15 +38,10 @@ wn_cost <- function(et, datosfda, linear_vars, si_vars, tt, sp, dfs, datosx) {
                           weights= ~survey_wt, data = dat, nest = TRUE)
   
   objetofda= fdata(datosfda, argvals = tt)
-  formula<- paste(paste(linear_vars,collapse="+"),paste(colnames(bs_data),collapse="+"),sep="+")
+  formula<- paste(formula_lv, paste(colnames(bs_data),collapse="+"),sep="+")
   source("survey2wassersteinmodel.R", local =knitr::knit_global())
   res=  survey2wassersteinmodel(formula=formula, data_analysis_svy=data_analysis_svy, objetofda = objetofda)
   Yhat<- res$predicciones
-  gx<- apply(Yhat, 2, mean)*dim(Yhat)[1]
-  source("cuadratico.R", local =knitr::knit_global())
-  if(any(diff(gx) < 0)) {
-    Yhat<- cuadratico(Yhat)
-  }
   
   #get array of Wasserstein distances between the response and its prediction
   RSS <- sapply(1:nrow(datosfda), function(i) fdadensity:::trapzRcpp(X = tt, Y = (datosfda[i, ] - Yhat[i, ])^2))
