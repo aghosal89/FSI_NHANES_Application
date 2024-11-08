de352949cc2033949512a33638a6c1db749c910f
Aritra Ghosal <56138318+aghosal89@users.noreply.github.com>
2024-11-08 13:49:35 -0600

    wn_cost.R

--- wn_cost.R
+++ wn_cost.R
@@ -1,14 +1,13 @@
-
-## This is the cost function for estimating the index parameter
+## This is the cost function for estimating the single index parameter in the PLFSI model
 
 ## Inputs: 
 
-# 1) et            - index parameter in polar coordinates
+# 1) et            - index parameter in polar coordinates.
 # 2) datosfda      - an nxm matrix as the response whose each row represents an 
 #                    observation, each column represents the quantile corresponding 
 #                    to a grid of t from [0,1].
 # 3) linear_vars   - the names of the p covariates for the linear part of model 
-#                    included in datosx
+#                    included in datosx.
 # 4) formula_lv    - a single character denoting the formula for the variables in the 
 #                    linear part to be considered for the model, e.g. interactions, 
 #                    higher order terms etc.
@@ -22,14 +21,19 @@
 
 ## Output: the mean square prediction error. 
 
+## R libraries required to run this function:
+
+# 1) survey
+# 2) splines
+# 3) fda.usc
+# 4) fdadensity
+
+## Other functions needed to be sourced from this repository to run this function:
+
+# 'survey2wassersteinmodel_2.R'
+
 wn_cost <- function(et, datosfda, linear_vars, formula_lv, si_vars, tt, sp, dfs, datosx) {
   
-  library("survey")
-  library("splines")
-  library("fda.usc")
-  library("fdadensity")
-  
-  source("polar2cart.R", local = knitr::knit_global())
   th<- as.matrix(polar2cart(et, 1))
   bs_fun<- as.matrix(datosx[,c(si_vars)])%*% th
   
@@ -37,18 +41,18 @@ wn_cost <- function(et, datosfda, linear_vars, formula_lv, si_vars, tt, sp, dfs,
   colnames(bs_data)<- paste("BS", seq(1:ncol(bs_data)), sep ="")
   datosx<- data.frame(cbind.data.frame(datosx, bs_data))
   
-  dat<- cbind.data.frame(datosx, datosfda)
+  dat <- cbind.data.frame(datosx, datosfda)
   data_analysis_svy <- svydesign(id= ~survey_id, strata = ~survey_strata, 
                           weights= ~survey_wt, data = dat, nest = TRUE)
   
   objetofda= fdata(datosfda, argvals = tt)
-  formula<- paste(formula_lv, paste(colnames(bs_data),collapse="+"),sep="+")
-  source("survey2wassersteinmodel.R", local =knitr::knit_global())
-  res=  survey2wassersteinmodel(formula=formula, data_analysis_svy=data_analysis_svy, objetofda = objetofda)
+  formula<- paste(formula_lv, paste(colnames(bs_data), collapse="+"), sep="+")
+  res=  survey2wassersteinmodel_2(formula= formula, data_analysis_svy= data_analysis_svy,
+                                  objetofda = objetofda)
   Yhat<- res$predicciones
   
   #get array of Wasserstein distances between the response and its prediction
-  RSS <- sapply(1:nrow(datosfda), function(i) fdadensity:::trapzRcpp(X = tt, Y = (datosfda[i, ] - Yhat[i, ])^2))
+  RSS <- sapply(1:nrow(datosfda), function(i) fdadensity:::trapzRcpp(X = tt, Y= (datosfda[i,] - Yhat[i,])^2))
   
   w<- data_analysis_svy$variables$survey_wt
   return(sum(w*RSS/sum(w)))
